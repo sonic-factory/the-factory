c7430e7582ea4d14c2cbd7302273c411509a27c1
Kristian <kristianism@pm.me>
2025-08-14 23:36:32 +0800

    Create yield farm contracts and factory

---
+++ src/yield/standardYieldFarm/StandardYieldFarm.sol
@@ -0,0 +1,7 @@
+//SPDX-License-Identifier: BSL 1.1
+pragma solidity 0.8.28;
+
+contract StandardYieldFarm {
+    // TODO: Implement the StandardYieldFarm contract logic.
+    // This contract will handle yield farming functionalities.
+}
