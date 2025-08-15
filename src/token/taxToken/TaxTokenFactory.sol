bb3b2c39b63cc2ce0c7f706bf67f3503da07d797
Kristian <kristianism@pm.me>
2025-08-15 16:16:57 +0800

    Update tax token factory contract to latest changes

--- src/token/taxToken/TaxTokenFactory.sol
+++ src/token/taxToken/TaxTokenFactory.sol
@@ -24,30 +24,43 @@ contract TaxTokenFactory is
 {
     using SafeERC20 for IERC20;
 
+    /// @notice Information of each Tax Token
+    struct TaxTokenInfo {
+        address tokenAddress;
+        address creator;
+        string name;
+        string symbol;
+        uint256 tokenId;
+    }
+
     /// @notice The address of the token implementation contract.
-    address public immutable tokenImplementation;
+    address public immutable taxTokenImplementation;
     /// @notice The fee to create a new token.
     uint256 public creationFee;
     /// @notice The number of tokens created.
     uint256 public tokenCounter;
 
     /// @notice Mapping from token ID to token address.
-    mapping(uint256 tokenId => address tokenAddress) public IdToAddress;
+    mapping(uint256 tokenId => address tokenAddress) internal IdToAddress;
+    /// @notice Mapping from creator address to their tax token addresses.
+    mapping(address creator => address[] taxTokens) internal creatorToTaxToken;
+    /// @notice Mapping from Tax Token address to its registry information.
+    mapping(address taxToken => TaxTokenInfo info) internal taxTokenInfo;
 
     /// @notice Constructor arguments for the token factory.
-    /// @param _tokenImplementation This is the address of the token to be cloned.
+    /// @param _taxTokenImplementation This is the address of the token to be cloned.
     /// @param _initialOwner The initial owner of the contract.
     /// @param _feeCollector The address that collects the fees.
     /// @param _creationFee The amount to collect for every contract creation.
     constructor(
-        address _tokenImplementation,
+        address _taxTokenImplementation,
         address _initialOwner,
         address _feeCollector,
         uint256 _creationFee
     ) CollectorHelper(_initialOwner, _feeCollector) {
-        if (_tokenImplementation == address(0)) revert ZeroAddress();
+        if (_taxTokenImplementation == address(0)) revert ZeroAddress();
 
-        tokenImplementation = _tokenImplementation;
+        taxTokenImplementation = _taxTokenImplementation;
         creationFee = _creationFee;
 
         _pause();
@@ -68,28 +81,44 @@ contract TaxTokenFactory is
         string memory symbol,
         uint256 initialSupply,
         uint256 transferTaxRate,
-        address taxBeneficiary,
-        address developer
-    ) external payable whenNotPaused nonReentrant returns (address token) {
-        if (developer == address(0) || taxBeneficiary == address(0)) revert ZeroAddress();
-        if (msg.value != creationFee) revert InvalidFee();
+        address taxBeneficiary
+    ) external payable whenNotPaused nonReentrant returns (address taxToken) {
+        if(bytes(name).length == 0 || bytes(symbol).length == 0) revert InputCannotBeNull();
+        if(taxBeneficiary == address(0)) revert ZeroAddress();
+        if(msg.value < creationFee) revert InvalidFee();
+
+        uint256 excessEth = msg.value - creationFee;
+
+        if (excessEth > 0) {
+            (bool success, ) = msg.sender.call{value: excessEth}("");
+            require(success, "Failed to refund excess ETH");
+        }
 
         tokenCounter = tokenCounter + 1;
 
-        token = Clones.clone(tokenImplementation);
+        taxToken = Clones.clone(taxTokenImplementation);
 
-        TaxToken(token).initialize(
+        TaxToken(taxToken).initialize(
             name,
             symbol,
             initialSupply,
             transferTaxRate,
             taxBeneficiary,
-            developer
+            msg.sender
         );
 
-        IdToAddress[tokenCounter] = token;
+        IdToAddress[tokenCounter] = taxToken;
+        creatorToTaxToken[msg.sender].push(taxToken);
 
-        emit TokenCreated(token, msg.sender);
+        taxTokenInfo[taxToken] = TaxTokenInfo({
+            tokenAddress: taxToken,
+            creator: msg.sender,
+            name: name,
+            symbol: symbol,
+            tokenId: tokenCounter
+        });
+
+        emit TaxTokenCreated(taxToken, msg.sender);
     }
 
     /// @notice This function sets the creation fee.
@@ -108,8 +137,31 @@ contract TaxTokenFactory is
         _unpause();
     }
 
-    /// @notice This function allows the UI to get a token address by its ID.
-    function getTokenById(uint256 tokenId) external view returns (address) {
+    /// @notice Get the total number of Tax Tokens created.
+    function getTotalTaxTokens() external view returns (uint256) {
+        return tokenCounter;
+    }
+
+    /// @notice Get the Tax Token address by its ID.
+    /// @param tokenId The ID of the token to retrieve.
+    function getTaxTokenById(uint256 tokenId) external view returns (address) {
         return IdToAddress[tokenId];
     }
-}
+
+    /// @notice Get all Tax Tokens created by a specific creator.
+    /// @param creator The address of the creator to retrieve tokens for.
+    function getTaxTokensByCreator(address creator) external view returns (address[] memory) {
+        return creatorToTaxToken[creator];
+    }
+
+    /// @notice Get the Tax Token information by its address.
+    /// @param taxToken The address of the tax token to retrieve information for.
+    function getTaxTokenInfo(address taxToken) external view returns (TaxTokenInfo memory) {
+        return taxTokenInfo[taxToken];
+    }
+
+    /// @notice Validates if the Tax Token address is valid.
+    /// @param taxToken The address of the tax token to validate.
+    function isValidTaxToken(address taxToken) external view returns (bool) {
+        return taxTokenInfo[taxToken].tokenAddress == taxToken;
+    }
