9cc4620d22569c3f48e527e4d355cf1f8238813c
Kristian <kristianism@pm.me>
2025-08-18 13:33:50 +0800

    Initial commit for the Yield Farm module

--- src/yield/standardYieldFarm/StandardYieldFarm.sol
+++ src/yield/standardYieldFarm/StandardYieldFarm.sol
@@ -1,7 +1,276 @@
-//SPDX-License-Identifier: BSL 1.1
+// SPDX-License-Identifier: BSL 1.1
 pragma solidity 0.8.28;
 
-contract StandardYieldFarm {
-    // TODO: Implement the StandardYieldFarm contract logic.
-    // This contract will handle yield farming functionalities.
+import "@openzeppelin/contracts-upgradeable/proxy/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/utils/ReentrancyGuardUpgradeable.sol";
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+
+import "@common/FactoryErrors.sol";
+import "@common/FactoryEvents.sol";
+
+contract StandardYieldFarm is 
+    Initializable,
+    OwnableUpgradeable,
+    ReentrancyGuardUpgradeable,
+    FactoryErrors,
+    FactoryEvents
+{
+    using SafeERC20 for IERC20;
+
+    // Info of each user.
+    struct UserInfo {
+        uint256 amount;
+        uint256 rewardDebt;
+    }
+
+    // Info of each pool.
+    struct PoolInfo {
+        IERC20 lpToken;
+        uint256 allocPoint;
+        uint256 lastRewardSecond;
+        uint256 accRewardPerShare;
+        uint256 depositFeeBP;
+    }
+
+    // The reward token
+    IERC20 public rewardToken;
+    // Dev address.
+    address public devAddress;
+    // reward tokens created/distributed per block.
+    uint256 public rewardPerSecond;
+    // Bonus muliplier for early makers.
+    uint256 public constant BONUS_MULTIPLIER = 1;
+    // Deposit Fee address
+    address public feeAddress;
+
+    // Info of each pool.
+    PoolInfo[] public poolInfo;
+    // Info of each user that stakes LP tokens.
+    mapping(uint256 => mapping(address => UserInfo)) public userInfo;
+    // Total allocation points. Must be the sum of all allocation points in all pools.
+    uint256 public totalAllocPoint = 0;
+    // The block number when reward mining starts.
+    uint256 public startTimestamp;
+
+    /// @notice Disables the ability to call the initializer
+    constructor() {
+        _disableInitializers();
+    }
+
+    function poolLength() external view returns (uint256) {
+        return poolInfo.length;
+    }
+
+    mapping(IERC20 => bool) public poolExistence;
+    modifier nonDuplicated(IERC20 _lpToken) {
+        require(poolExistence[_lpToken] == false, "nonDuplicated: duplicated");
+        _;
+    }
+
+    function initialize(
+        IERC20 _rewardToken,
+        address _devAddress,
+        address _feeAddress,
+        uint256 _rewardPerSecond,
+        uint256 _startTimestamp
+    ) external initializer {
+        __Ownable_init(_devAddress);
+        __ReentrancyGuard_init();
+
+        rewardToken = _rewardToken;
+        devAddress = _devAddress;
+        feeAddress = _feeAddress;
+        rewardPerSecond = _rewardPerSecond;
+        startTimestamp = _startTimestamp;
+    }
+
+    // Add a new lp to the pool. Can only be called by the owner.
+    function add(
+        uint256 _allocPoint,
+        IERC20 _lpToken,
+        uint256 _depositFeeBP,
+        bool _withUpdate
+    ) 
+        public
+        onlyOwner
+        nonDuplicated(_lpToken)
+    {
+        require(_depositFeeBP <= 10000, "add: invalid deposit fee basis points");
+        
+        if (_withUpdate) {
+            massUpdatePools();
+        }
+
+        uint64 lastRewardSecond = block.timestamp > startTimestamp ? block.timestamp : startTimestamp;
+        totalAllocPoint = totalAllocPoint + _allocPoint;
+        poolExistence[_lpToken] = true;
+        poolInfo.push(PoolInfo({
+        lpToken : _lpToken,
+        allocPoint : _allocPoint,
+        lastRewardSecond : lastRewardSecond,
+        accRewardPerShare : 0,
+        depositFeeBP : _depositFeeBP
+        }));
+    }
+
+    // Update the given pool's reward allocation point and deposit fee. Can only be called by the owner.
+    function set(uint256 _pid, uint256 _allocPoint, uint16 _depositFeeBP, bool _withUpdate) public onlyOwner {
+        require(_depositFeeBP <= 10000, "set: invalid deposit fee basis points");
+        
+        if (_withUpdate) {
+            massUpdatePools();
+        }
+
+        totalAllocPoint = totalAllocPoint - poolInfo[_pid].allocPoint + _allocPoint;
+        poolInfo[_pid].allocPoint = _allocPoint;
+        poolInfo[_pid].depositFeeBP = _depositFeeBP;
+    }
+
+    // Return reward multiplier over the given _from to _to second.
+    function getMultiplier(uint256 _from, uint256 _to) public view returns (uint256) {
+        return (_to - _from) * BONUS_MULTIPLIER;
+    }
+
+    // View function to see pending rewards on frontend.
+    function pendingReward(uint256 _pid, address _user) external view returns (uint256) {
+        PoolInfo storage pool = poolInfo[_pid];
+        UserInfo storage user = userInfo[_pid][_user];
+        uint256 accRewardPerShare = pool.accRewardPerShare;
+        uint256 lpSupply = pool.lpToken.balanceOf(address(this));
+        
+        if (block.timestamp > pool.lastRewardSecond && lpSupply != 0) {
+            uint256 multiplier = getMultiplier(pool.lastRewardSecond, block.timestamp);
+            uint256 reward = (multiplier * rewardPerSecond * pool.allocPoint) / totalAllocPoint;
+            accRewardPerShare = accRewardPerShare + (reward * 1e18) / lpSupply;
+        }
+
+        return (user.amount * accRewardPerShare) / 1e18 - user.rewardDebt;
+    }
+
+    // Update reward variables for all pools. Be careful of gas spending!
+    function massUpdatePools() public {
+        uint256 length = poolInfo.length;
+        
+        for (uint256 pid = 0; pid < length; ++pid) {
+            updatePool(pid);
+        }
+    }
+
+    // Update reward variables of the given pool to be up-to-date.
+    function updatePool(uint256 _pid) public {
+        PoolInfo storage pool = poolInfo[_pid];
+        
+        if (block.timestamp <= pool.lastRewardSecond) {
+            return;
+        }
+
+        uint256 lpSupply = pool.lpToken.balanceOf(address(this));
+        
+        if (lpSupply == 0 || pool.allocPoint == 0) {
+            pool.lastRewardSecond = block.timestamp;
+            return;
+        }
+
+        uint256 multiplier = getMultiplier(pool.lastRewardSecond, block.timestamp);
+        uint256 reward = (multiplier * rewardPerSecond * pool.allocPoint) / totalAllocPoint;
+
+        // For a generic reward token we do NOT mint here;
+        // the contract is expected to be funded with rewardToken tokens beforehand.
+        pool.accRewardPerShare = pool.accRewardPerShare + (reward * 1e18) / lpSupply;
+        pool.lastRewardSecond = block.timestamp;
+    }
+
+    // Deposit LP tokens to MasterChef for reward allocation.
+    function deposit(uint256 _pid, uint256 _amount) public nonReentrant {
+        PoolInfo storage pool = poolInfo[_pid];
+        UserInfo storage user = userInfo[_pid][msg.sender];
+        updatePool(_pid);
+
+        if (user.amount > 0) {
+            uint256 pending = (user.amount * pool.accRewardPerShare) / 1e18 - user.rewardDebt;
+            if (pending > 0) {
+                safeRewardTransfer(msg.sender, pending);
+            }
+        }
+
+        if (_amount > 0) {
+            pool.lpToken.safeTransferFrom(address(msg.sender), address(this), _amount);
+            if (pool.depositFeeBP > 0) {
+                uint256 depositFee = (_amount * pool.depositFeeBP) / 10000;
+                pool.lpToken.safeTransfer(feeAddress, depositFee);
+                user.amount = user.amount + _amount - depositFee;
+            } else {
+                user.amount = user.amount + _amount;
+            }
+        }
+
+        user.rewardDebt = (user.amount * pool.accRewardPerShare) / 1e18;
+        emit Deposit(msg.sender, _pid, _amount);
+    }
+
+    // Withdraw LP tokens from the yield farm.
+    function withdraw(uint256 _pid, uint256 _amount) public nonReentrant {
+        PoolInfo storage pool = poolInfo[_pid];
+        UserInfo storage user = userInfo[_pid][msg.sender];
+        require(user.amount >= _amount, "withdraw: not good");
+        updatePool(_pid);
+        uint256 pending = (user.amount * pool.accRewardPerShare) / 1e18 - user.rewardDebt;
+        if (pending > 0) {
+            safeRewardTransfer(msg.sender, pending);
+        }
+
+        if (_amount > 0) {
+            user.amount = user.amount - _amount;
+            pool.lpToken.safeTransfer(address(msg.sender), _amount);
+        }
+
+        user.rewardDebt = (user.amount * pool.accRewardPerShare) / 1e18;
+        emit Withdraw(msg.sender, _pid, _amount);
+    }
+
+    // Withdraw without caring about rewards. EMERGENCY ONLY.
+    function emergencyWithdraw(uint256 _pid) public nonReentrant {
+        PoolInfo storage pool = poolInfo[_pid];
+        UserInfo storage user = userInfo[_pid][msg.sender];
+        uint256 amount = user.amount;
+        user.amount = 0;
+        user.rewardDebt = 0;
+        pool.lpToken.safeTransfer(address(msg.sender), amount);
+        emit EmergencyWithdraw(msg.sender, _pid, amount);
+    }
+
+    // Safe reward transfer function, just in case if rounding error causes pool to not have enough tokens.
+    function safeRewardTransfer(address _to, uint256 _amount) internal {
+        uint256 bal = rewardToken.balanceOf(address(this));
+        bool transferSuccess = false;
+
+        if (_amount > bal) {
+            transferSuccess = rewardToken.transfer(_to, bal);
+        } else {
+            transferSuccess = rewardToken.transfer(_to, _amount);
+        }
+
+        require(transferSuccess, "safeRewardTransfer: transfer failed");
+    }
+
+    function setDevAddress(address _devAddress) public {
+        require(msg.sender == devAddress, "dev: wut?");
+        devAddress = _devAddress;
+        emit SetDevAddress(msg.sender, _devAddress);
+    }
+
+    function setFeeAddress(address _feeAddress) public {
+        require(msg.sender == feeAddress, "setFeeAddress: FORBIDDEN");
+        feeAddress = _feeAddress;
+        emit SetFeeAddress(msg.sender, _feeAddress);
+    }
+
+    function updateEmissionRate(uint256 _rewardPerSecond) public onlyOwner {
+        massUpdatePools();
+        rewardPerSecond = _rewardPerSecond;
+        emit UpdateEmissionRate(msg.sender, _rewardPerSecond);
+    }
 }
--- src/yield/standardYieldFarm/StandardYieldFarmFactory.sol
+++ src/yield/standardYieldFarm/StandardYieldFarmFactory.sol
@@ -1,7 +1,191 @@
 //SPDX-License-Identifier: BSL 1.1
 pragma solidity 0.8.28;
 
-contract StandardYieldFarmFactory {
-    // TODO: Implement the StandardYieldFarmFactory contract logic.
-    // This factory will create instances of StandardYieldFarm contracts.
+import "@openzeppelin/contracts/proxy/Clones.sol";
+import "@openzeppelin/contracts/utils/Pausable.sol";
+import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+
+import "@standardYield/StandardYieldFarm.sol";
+import "@common/CollectorHelper.sol";
+
+/**
+ * @title Standard Yield Farm Factory
+ * @notice This is a factory for creating Standard Yield Farm contracts.
+ * @dev Proxy implementation are Clones. Implementation is immutable and not upgradeable.
+ */
+contract StandardYieldFarmFactory is 
+    Pausable, 
+    ReentrancyGuard,
+    FactoryErrors,
+    FactoryEvents,
+    CollectorHelper
+{
+    using SafeERC20 for IERC20;
+
+    /// @notice Information of each yield farm.
+    struct YieldFarmInfo {
+        address yieldFarmAddress;
+        address creator;
+        uint64 startTimestamp;
+        uint64 durationSeconds;
+        uint256 yieldFarmId;
+    }
+
+    /// @notice The address of the standard yield farm implementation contract.
+    address public immutable yieldFarmImplementation;
+    /// @notice The address of the standard token implementation contract.
+    address public immutable tokenImplementation;
+    /// @notice The fee to create a new yield farm.
+    uint256 public creationFee;
+    /// @notice The number of yield farms created.
+    uint256 internal yieldFarmCounter;
+
+    /// @notice Mapping from yield farm ID to yield farm address.
+    mapping(uint256 yieldFarmId => address yieldFarmAddress) internal IdToAddress;
+    /// @notice Mapping from creator address to their yield farm addresses.
+    mapping(address creator => address[] yieldFarm) internal creatorToLockers;
+    /// @notice Mapping from yield farm address to its registry information.
+    mapping(address yieldFarm => YieldFarmInfo info) internal yieldFarmInfo;
+
+    /**
+     * @notice Constructor arguments for the yield farm factory.
+     * @param _yieldFarmImplementation This is the address of the yield farm to be cloned.
+     * @param _initialOwner The initial owner of the factory.
+     * @param _feeCollector The address that will collect the creation fees.
+     * @param _creationFee The amount to collect for every contract creation.
+     */
+    constructor(
+        address _yieldFarmImplementation,
+        address _initialOwner,
+        address _feeCollector,
+        uint256 _creationFee
+    ) CollectorHelper(_initialOwner, _feeCollector) {
+        if(_yieldFarmImplementation == address(0)) revert ZeroAddress();
+
+        lockerImplementation = _yieldFarmImplementation;
+        creationFee = _creationFee;
+
+        _pause();
+    }
+
+    /// @notice This function allows the contract to receive ETH.
+    receive() external payable {}
+
+    /**
+     * @notice This function is called to create a new yield farm contract.
+     * @param _startTimestamp The timestamp when the vesting starts.
+     * @param _durationSeconds The duration in seconds for which the tokens will be vested.
+     * @param _isNative A boolean indicating if the locker is for native tokens.
+     * @param _token The address of the token to be vested (if not native).
+     * @param _amount The amount of tokens to be vested. 
+    */
+    function createYieldFarm(
+        uint64 _startTimestamp,
+        uint64 _durationSeconds,
+        bool _isNative,
+        address _token,
+        uint256 _amount
+    ) external payable whenNotPaused nonReentrant returns (address payable yieldFarm) {
+        if(_startTimestamp < block.timestamp) revert InvalidTimestamp();
+        if(msg.value < creationFee) revert InvalidFee();
+
+        yieldFarmCounter = yieldFarmCounter + 1;
+
+        yieldFarm = payable(Clones.clone(lockerImplementation));
+
+        Vesting(yieldFarm).initialize(
+            msg.sender,
+            _startTimestamp,
+            _durationSeconds
+        );
+
+        IdToAddress[yieldFarmCounter] = yieldFarm;
+        creatorToLockers[msg.sender].push(yieldFarm);
+
+        yieldFarmInfo[yieldFarm] = YieldFarmInfo({
+            yieldFarmAddress: yieldFarm,
+            creator: msg.sender,
+            startTimestamp: _startTimestamp,
+            durationSeconds: _durationSeconds,
+            yieldFarmId: yieldFarmCounter
+        });
+
+        if (_isNative == true) {
+            // Transfer ETH to the yield farm if it is native.
+            require(msg.value >= (creationFee + _amount), InvalidFee());
+
+            (bool success, ) = yieldFarm.call{value: _amount}("");
+            require(success, "Failed to send ETH");
+
+            // Refund excess ETH if any.
+            uint256 excessNative = msg.value - (creationFee + _amount);
+            if (excessNative > 0) {
+                (bool excessSuccess, ) = msg.sender.call{value: excessNative}("");
+                require(excessSuccess, "Failed to refund excess ETH");
+            }
+        } else {
+            // Transfer tokens to the yield farm if not native.
+            require(_token != address(0), InvalidAddress());
+            require(_amount > 0, ZeroAmount());
+
+            IERC20(_token).safeTransferFrom(msg.sender, yieldFarm, _amount);
+
+            // Refund excess ETH if any.
+            uint256 excessNative = msg.value - creationFee;
+            if (excessNative > 0) {
+                (bool excessSuccess, ) = msg.sender.call{value: excessNative}("");
+                require(excessSuccess, "Failed to refund excess ETH");
+            }
+        }
+
+        emit YieldFarmCreated(yieldFarm, msg.sender, _startTimestamp, _durationSeconds, lockerCounter);
+    }
+
+    /// @notice This function sets the creation fee.
+    /// @param _creationFee The amount to set as the creation fee.
+    function setCreationFee(uint256 _creationFee) external onlyOwner {       
+        creationFee = _creationFee;
+        emit CreationFeeUpdated(_creationFee);
+    }
+
+    /// @notice This function allows the owner to pause the contract.
+    function pause() external onlyOwner {
+        _pause();
+    }
+
+    /// @notice This function allows the owner to unpause the contract.
+    function unpause() external onlyOwner {
+        _unpause();
+    }
+
+    /// @notice Get the total number of lockers created.
+    function getTotalLockers() external view returns (uint256) {
+        return lockerCounter;
+    }
+
+    /// @notice  Get the locker address by its ID.
+    /// @param lockerId The ID of the locker to retrieve.
+    function getLockerById(uint256 lockerId) external view returns (address) {
+        return IdToAddress[lockerId];
+    }
+
+    /// @notice Get all lockers created by a specific creator.
+    /// @param creator The address of the creator to retrieve lockers for.
+    function getLockersByCreator(address creator) external view returns (address[] memory) {
+        return creatorToLockers[creator];
+    }
+
+    /// @notice Get the locker information by its address.
+    /// @param locker The address of the locker to retrieve information for.
+    function getLockerInfo(address locker) external view returns (LockerInfo memory) {
+        return lockerInfo[locker];
+    }
+
+    /// @notice Validates if the locker address is valid.
+    /// @param locker The address of the locker to validate.
+    function isValidLocker(address locker) external view returns (bool) {
+        return lockerInfo[locker].lockerAddress == locker;
+    }
 }
